df740f05afa923dd76d070e2f2333201a9212237
Sarka Chwastkova <chwastkova.s@gmail.com>
2022-04-24 09:53:01 +0200

    fix: database config

--- config/database.js
+++ config/database.js
@@ -1,11 +1,52 @@
-const path = require('path');
+const parse =
+  process.env.NODE_ENV === "development"
+    ? () => ""
+    : require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+const devConnections = (env) => {
+  return {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+  };
+};
+
+const productionConnections = (env) => {
+  return {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        },
+      },
+      options: {
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        },
+      },
+    },
+  };
+};
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
+  defaultConnection: "default",
+  connections:
+    env("NODE_ENV") === "development"
+      ? devConnections(env)
+      : productionConnections(env),
 });
